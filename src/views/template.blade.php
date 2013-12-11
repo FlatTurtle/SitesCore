3eac5457312e29afb4d7f0b627396e070a8e8762
Jens Segers <segers.jens@gmail.com>
2013-12-11 19:28:00 +0100

    Fixing bad css filename

--- src/views/template.blade.php
+++ src/views/template.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $flatturtle->title }}</title>
-    <link href="{{ URL::asset('packages/flatturtle/sitecore/css/common.css' . filemtime(public_path() . '/packages/flatturtle/sitecore/css/common.css')) }}" rel="stylesheet">
+    <link href="{{ URL::asset('packages/flatturtle/sitecore/css/common.css?v=' . filemtime(public_path() . '/packages/flatturtle/sitecore/css/common.css')) }}" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="https://fast.fonts.com/cssapi/66253153-9c89-413c-814d-60d3ba0d6ac2.css"/>
 
     @if (File::exists(public_path() . '/favicon.ico'))
