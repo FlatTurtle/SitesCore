66de41db57727461d05001bc4083c17f56341750
Jens Segers <segers.jens@gmail.com>
2014-02-18 11:06:54 +0100

    Adding temp lang files to french

---
+++ src/lang/fr/reservations.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+    'title' => 'Reservations',
+    'subtitle' => 'Click on an item below to book a meeting room.',
+    'date' => 'Select a date',
+    'click-me' => 'Click me',
+    'time' => 'Select time period',
+    'details' => 'Enter your details',
+    'from' => 'From:',
+    'to' => 'To:',
+
+    'company' => 'Company:',
+    'email' => 'Email:',
+    'subject' => 'Subject:',
+    'announce' => 'Speakers:',
+    'comment' => 'Description (optional)',
+    'button' => 'Submit reservation',
+
+    'success' => 'Reservation created',
+
+    // Exceptions
+    'error' => 'Something went wrong.',
+    'thing-unavailable' => 'Unable to book at that time.',
+    'invalid-credentials' => 'The credentials you provided are invalid.',
+
+);
