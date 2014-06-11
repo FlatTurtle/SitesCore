1f83c6ec84b7c8d813605007d5061bd7d106f283
Jens Segers <segers.jens@gmail.com>
2014-06-11 18:28:12 +0200

    Fix cache problem for php content

--- src/models/Content.php
+++ src/models/Content.php
@@ -12,24 +12,22 @@ class Content extends Model {
 
 	public static function all()
 	{
-		// Get directory from config
-		$directory = Config::get('sitecore::directory', 'content');
-
-		// Get content files
-		$files = File::files(base_path() . '/' . $directory);
-
-		$models = array();
-
-		// Loop all files
-		foreach ($files as $file)
+		// Cache the directory listing
+		return Cache::rememberForever('flatturtle.files', function()
 		{
-			// Generate cache based on file modification date
-			$cache_key = 'flatturtle.' . md5($file . filemtime($file));
+			// Get directory from config
+			$directory = Config::get('sitecore::directory', 'content');
 
-			// Try to get model from cache
-			$model = Cache::rememberForever($cache_key, function() use ($file)
+			// Get content files
+			$files = File::files(base_path() . '/' . $directory);
+
+			// The model collection
+			$models = new Collection;
+
+			// Loop all files
+			foreach ($files as $file)
 			{
-			    // Create block id based on file name
+			    // Create id based on file name
 				$id = pathinfo($file, PATHINFO_FILENAME);
 				$id = preg_replace('#[0-9]+-#', '', $id);
 				$id = str_replace(' ', '-', $id);
@@ -38,36 +36,15 @@ class Content extends Model {
 				// Create a new model
 				$model = new Content;
 				$model->id = $id;
+				$model->file = $file;
 				$model->type = pathinfo($file, PATHINFO_EXTENSION);
 
-				// Parse markdown
-				if ($model->type == 'md')
-				{
-					$html = File::get($file);
-					$model->html = Parsedown::instance()->parse($html);
-				}
-				// Include php files
-				else if ($model->type == 'php')
-				{
-					// Filename without .blade.php
-					$view = str_replace('.blade', '', pathinfo($file, PATHINFO_FILENAME));
+				// Add model to list
+				$models->push($model);
+			}
 
-					$model->html = (string) View::make($view);
-				}
-				// Other files
-				else
-				{
-					$model->html = File::get($file);
-				}
-
-				return $model;
-			});
-
-			// Add model to list
-			$models[] = $model;
-		}
-
-		return new Collection($models);
+			return $models;
+		});
 	}
 
 	/*
@@ -85,7 +62,32 @@ class Content extends Model {
 		{
 			return strip_tags($matches[1]);
 		}
+	}
 
+	public function getHtmlAttribute()
+	{
+		// Parse markdown
+		if ($this->type == 'md')
+		{
+			$model = $this;
+
+			// Cache parsed markdown forever
+			return Cache::rememberForever('flatturtle.file.{$this->id}', function() use($model)
+			{
+				$html = File::get($model->file);
+				return Parsedown::instance()->parse($html);
+			});
+		}
+
+		// Include php files
+		else if ($this->type == 'php')
+		{
+			// Filename without .blade.php
+			$view = str_replace('.blade', '', pathinfo($this->file, PATHINFO_FILENAME));
+			return (string) View::make($view);
+		}
+
+		return File::get($this->file);
 	}
 
 }
